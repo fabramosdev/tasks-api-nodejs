4163d4224b0bc0effe7a2a05aa8f61be3c094723
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-12-28 23:01:15 -0300

    nodemon/express added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const routes = require('./routes')
+
+const app = express();
+
+app.use(routes)
+
+app.listen(8080, () => {
+    console.log('Server started at port 8080')
+})
